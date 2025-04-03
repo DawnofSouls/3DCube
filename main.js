d794238dae049c89d2723c6945a8fb02077b91b6
DawnofSouls <28073955+DawnofSouls@users.noreply.github.com>
2025-04-02 18:33:25 -0700

    Added grid and changed camera pos

--- main.js
+++ main.js
@@ -10,11 +10,20 @@ const camera = new THREE.PerspectiveCamera(
   1000                                      // FAR
 );
 
-
+//
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+//
 
+//grid
+const size = 50;
+const divisions = 100;
+const gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
+
+//camera position
+camera.position.y = 1;
 
 //renders everything to show up on screen
 renderer.render( scene, camera );
