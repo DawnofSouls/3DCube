4c2c0d9838bf812d3b9fa1876975158bc4057038
DawnofSouls <28073955+DawnofSouls@users.noreply.github.com>
2025-04-02 18:29:02 -0700

    Added code to make three.js work

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 import * as THREE from 'three';
+
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
+const camera = new THREE.PerspectiveCamera(
+  75,                                       // FOV
+  window.innerWidth / window.innerHeight,   // ASPECT RATIO
+  0.1,                                      // NEAR 
+  1000                                      // FAR
+);
+
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+//renders everything to show up on screen
+renderer.render( scene, camera );
+
